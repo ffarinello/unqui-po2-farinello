70bdb04ae78141bca4d7847241daf6758f644fa5
SeSocio <SeSocio@PF20TYE0>
2022-05-11 19:00:40 -0300

    tp4

---
+++ src/ar/edu/unq/po2/tp4/ejercicio1/Cliente.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tp4.ejercicio1;
+
+public class Cliente {
+
+}
---
+++ src/ar/edu/unq/po2/tp4/module-info.java
@@ -0,0 +1,21 @@
+
+/*
+ class Caja
+
+ *	registra el producto {
+      consultar precio (del producto) -> get precio
+ 				    acumular el monto -> sumar precio a total
+ 	descontar el producto (del stock) -> -1 en stock 
+ 	}
+ 	
+ *	obtiene el monto total a pagar (por el cliente)
+ *	informa el monto al cliente
+ *	
+
+
+Productos
+		** producto de cooperativa {
+				{ precioBase = precio + IVA(10%) }
+		
+		**producto de empresa tradicional
+*/	
