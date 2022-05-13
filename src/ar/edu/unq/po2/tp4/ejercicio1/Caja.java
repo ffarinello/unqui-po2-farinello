a1f13edf85beaa71dd9ce3e6f3377b310e73e166
Dalekesovo <Dalekesovo@TREMENDO>
2022-05-12 21:39:42 -0300

    CAJA - MercadoCentral - 1

--- src/ar/edu/unq/po2/tp4/ejercicio1/Caja.java
+++ src/ar/edu/unq/po2/tp4/ejercicio1/Caja.java
@@ -1,26 +1,35 @@
-package unqui.po2.tp4;
+package ar.edu.unq.po2.tp4.ejercicio1;
 
 import java.util.ArrayList;
 
 public class Caja {
 	
-	ArrayList<Producto> stock = new ArrayList<>();
+	ArrayList<Producto> listaDeProductos = new ArrayList<>();
 	
 	private float totalAPagar = 0;
-	
+
 	public void registrarProducto(Producto producto, Mercado mercado, Cliente cliente) {
 			/* Registrar un producto implica: 
 					* registrarlo en la nuevaCompra
 					* descontar el producto del stock del mercado
 					* 
 			*/
-		this.registrar(producto);
+		this.registrar(mercado, producto);
+		this.informarSaldo(cliente);
+		/*
 		mercado.descontarProducto(producto);
 		cliente.setSaldoAPagar(producto);
+		*/
 	}
 	
-	
-	public void registrar(Producto producto) {
+	public void registrar(Mercado mercado, Producto producto) {
+		listaDeProductos.add(producto);
 		totalAPagar += producto.getPrecio();
+		mercado.decrementarStock(producto);
 	}
+	
+	private void informarSaldo(Cliente cliente) {
+		cliente.saldoAPagar = totalAPagar;
+	}
+
 }
